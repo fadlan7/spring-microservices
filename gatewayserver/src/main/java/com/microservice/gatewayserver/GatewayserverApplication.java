0e54a8844ad033300d56ec6c8b8db37e213d30aa
Fadlan Sayyidul Anam <inifadlan7@gmail.com>
2024-12-12 11:26:52 +0700

    circuit breaker and retry pattern

--- gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservice/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -20,17 +22,25 @@ public class GatewayserverApplication {
         return routeLocatorBuilder.routes()
                 .route(p -> p
                         .path("/eazybank/accounts/**")
-                        .filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(conf -> conf.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport"))
+                        )
                         .uri("lb://ACCOUNTS"))
                 .route(p -> p
                         .path("/eazybank/loans/**")
-                        .filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)
+                                )
+                        )
                         .uri("lb://LOANS"))
                 .route(p -> p
                         .path("/eazybank/cards/**")
-                        .filters( f -> f.rewritePath("/eazybank/cards/(?<segment>.*)","/${segment}")
+                        .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
                                 .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
                         .uri("lb://CARDS")).build();
 
